b908e0e53659b378836e1b72cdc44f1ed1e692f1
ayeshaadnan1 <160243600+ayeshaadnan1@users.noreply.github.com>
2024-04-29 07:49:25 +0500

    Add files via upload

---
+++ sms.ts
@@ -0,0 +1,65 @@
+class School {
+  name: string;
+
+  student: Student[] = [];
+  teacher: Teacher[] = [];
+
+  addStudent(stdobj: Student) {
+    this.student.push(stdobj);
+  }
+  addTeacher(teobj: Teacher) {
+    this.teacher.push(teobj);
+  }
+  constructor(name: string) {
+    this.name = name;
+  }
+}
+
+class Student {
+  name: string;
+  age: number;
+  schoolName: string;
+
+  constructor(name: string, age: number, schoolName: string) {
+    this.name = name;
+    this.age = age;
+    this.schoolName = schoolName;
+  }
+}
+class Teacher extends Student {}
+
+//school
+let school1 = new School("Gulistan");
+let school2 = new School("Ayesha Bawani");
+let school3 = new School("PIAIC");
+
+//student
+let s1 = new Student("Ayesha Adnan", 4, school1.name);
+let s2 = new Student("Waleed", 8, school2.name);
+let s3 = new Student("Saad",10, school3.name);
+let s4 = new Student("Bia", 30, school3.name);
+
+//teacher
+let t1 = new Teacher("Madiha", 26, school1.name);
+let t2 = new Teacher("Fariha", 27, school2.name);
+let t3 = new Teacher("Farhanarr", 25, school3.name);
+
+//student data
+school1.addStudent(s1);
+school2.addStudent(s2);
+school3.addStudent(s3);
+school3.addStudent(s4);
+
+//teacher data
+school1.addTeacher(t1);
+school2.addTeacher(t2);
+school3.addTeacher(t3);
+
+console.log(t1);
+console.log(t2);
+console.log(t3);
+
+//school
+console.log(school1);
+console.log(school2);
+console.log(school3);
